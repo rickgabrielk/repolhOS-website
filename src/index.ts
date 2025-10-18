e00f15517951f0d657714e31a718fe2c6ab3dff9
Rick Gabriel <rickgabrielk@gmail.com>
2025-10-17 21:44:15 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 // Home route - HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'hom.html'))
+  res.sendFile(path.join(__dirname, '..', 'public', 'hom.css'))
 })
 
 /*app.get('/', (req, res) => {
