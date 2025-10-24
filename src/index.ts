e6ffb0c99330228da2df703eaa5887ea2678caed
Rick Gabriel <rickgabrielk@gmail.com>
2025-10-24 16:51:58 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,11 @@ const app = express()
 // Home rou
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'a.html'))
+  res.sendFile(path.join(__dirname, '..', 'public', 'h.html'))
 })
 
 app.get('/portuguese', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'h.html'))
+  res.sendFile(path.join(__dirname, '..', 'public', 'a.html'))
 })
 
 app.use((err, req, res, next) => {
